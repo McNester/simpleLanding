9c5dbd6490750717266f21786fc408c4c6e8ebd7
McNester <seraf5@bk.ru>
2022-07-26 01:20:47 +0600

    fixed bug

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
         </h1>
 
         <PersonCard v-for="person in peopleForCard" :key="person.id" :personsInf="person">
-          <img src="/imgSrc/firstPerson.png"> 
+          <img :src="this.source"> 
         </PersonCard>
 
       </div>
@@ -66,7 +66,8 @@ export default{
         {id:0,title:'24/7 support',content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit',imgSrc:'/imgSrc/lock.png'},
         {id:1,title:'1000+ of reviews',content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit',imgSrc:'/imgSrc/graphic.png'},
         {id:2,title:'And more!',content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit',imgSrc:'/imgSrc/trophy.png'}
-      ]
+      ],
+      source:'/imgSrc/firstPerson.png'
     }
   },
   methods:{
